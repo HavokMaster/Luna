a5aef12612fb6a82075d7a68dd04b02b3ad24685
HavokMaster <53501215+HavokMaster@users.noreply.github.com>
2024-04-04 21:03:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import speech_recognition as sr
+
 from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
@@ -12,7 +14,6 @@ ProcessManager = Functions.ProcessManager()
 loadPath(ProcessManager)
 
 llm = Ollama(model="mistral")
-
 
 response_schemas = [
     ResponseSchema(name="action", description="The most suitable action to achieve this task for the user"),
@@ -53,31 +54,62 @@ codePrompt = PromptTemplate(template=codeTemplate, input_variables=["query"])
 chatPrompt = PromptTemplate(template=chatTemplate, input_variables=["query"])
 
 st.title("LUNA: Virtual Assistant")
-user_prompt = st.text_area("Enter your prompt")
+user_prompt = ""
+
+def listen_to_microphone():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    try:
+        st.write("Transcribing...")
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        st.write("Sorry, I could not understand what you said.")
+        return ""
+    except sr.RequestError as e:
+        st.write("Sorry, there was an error in processing your request. Please try again later.")
+        return ""
+
+def process_user_input(user_prompt):
+    finalPrompt = prompt.format(query=user_prompt, format_instructions=format_instructions)
+    output = llm.invoke(finalPrompt)
+    data = output_parser.parse(output)
+
+    if data['action'] == 'SearchWeb':
+        st.write("Search Web for:", data['parameters'])
+        searchWeb(data['parameters'])
+    elif data['action'] == 'WriteCode':
+        st.write("Sure, I will write the code for you!")
+        codeOutput = llm.invoke(codePrompt.format(query=user_prompt))
+        st.write("Code API")
+        st.write(codeOutput)
+        lines = codeOutput.split('\n')
+        if lines and lines[0].startswith('`'): lines.pop(0)
+        if lines and lines[-1].startswith('`'): lines.pop()
+        codeOutput = '\n'.join(lines)
+        FileSystemManager.writeToFile("code.txt", codeOutput)
+    else:
+        st.write("[Chat Mode]")
+        st.write(llm.invoke(chatPrompt.format(query=user_prompt)))
+    
+    st.write("API Data (Currently in development)")
+    st.write(output)
+
+capture_voice = st.button("Capture Voice")
+if capture_voice:
+    user_prompt = listen_to_microphone()
+    st.text_area("Enter voice prompt", value=user_prompt, key="voice_prompt")
+    process_user_input(user_prompt)
+else:
+    user_prompt = st.text_area("Enter your prompt")
+
 button = st.button("Submit")
 
 if button:
-    if user_prompt:
-        finalPrompt = prompt.format(query=user_prompt, format_instructions=format_instructions)
-        output = llm.invoke(finalPrompt)
-        data = output_parser.parse(output)
-
-        if data['action'] == 'SearchWeb':
-            st.write("Search Web for:", data['parameters'])
-            searchWeb(data['parameters'])
-        elif data['action'] == 'WriteCode':
-            st.write("Sure, I will write the code for you!")
-            codeOutput = llm.invoke(codePrompt.format(query=user_prompt))
-            st.write("Code API")
-            st.write(codeOutput)
-            lines = codeOutput.split('\n')
-            lines.pop(0)
-            lines.pop()
-            codeOutput = '\n'.join(lines)
-            FileSystemManager.writeToFile("code.txt", codeOutput)
-        else:
-            st.write("[Chat Mode]")
-            st.write(llm.invoke(chatPrompt.format(query=user_prompt)))
-        
-        st.write("API Data (Currently in development)")
-        st.write(output)
+    if not user_prompt:
+        st.write("Please provide a prompt.")
+    else:
+        process_user_input(user_prompt)
--- mainOpenAI.py
+++ mainOpenAI.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import speech_recognition as sr
 from langchain_openai import OpenAI
 import os
 from langchain.prompts import PromptTemplate
@@ -6,6 +7,7 @@ from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 from macros import *
 from modules.Path import loadPath
 from modules import Functions
+
 os.environ["OPENAI_API_KEY"] = ""
 FileSystemManager = Functions.FileSystemManager()
 ProcessManager = Functions.ProcessManager()
@@ -53,31 +55,62 @@ codePrompt = PromptTemplate(template=codeTemplate, input_variables=["query"])
 chatPrompt = PromptTemplate(template=chatTemplate, input_variables=["query"])
 
 st.title("LUNA: Virtual Assistant")
-user_prompt = st.text_area("Enter your prompt")
+user_prompt = ""
+
+def listen_to_microphone():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    try:
+        st.write("Transcribing...")
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        st.write("Sorry, I could not understand what you said.")
+        return ""
+    except sr.RequestError as e:
+        st.write("Sorry, there was an error in processing your request. Please try again later.")
+        return ""
+
+def process_user_input(user_prompt):
+    finalPrompt = prompt.format(query=user_prompt, format_instructions=format_instructions)
+    output = llm.invoke(finalPrompt)
+    data = output_parser.parse(output)
+
+    if data['action'] == 'SearchWeb':
+        st.write("Search Web for:", data['parameters'])
+        searchWeb(data['parameters'])
+    elif data['action'] == 'WriteCode':
+        st.write("Sure, I will write the code for you!")
+        codeOutput = llm.invoke(codePrompt.format(query=user_prompt))
+        st.write("Code API")
+        st.write(codeOutput)
+        lines = codeOutput.split('\n')
+        if lines and lines[0].startswith('`'): lines.pop(0)
+        if lines and lines[-1].startswith('`'): lines.pop()
+        codeOutput = '\n'.join(lines)
+        FileSystemManager.writeToFile("code.txt", codeOutput)
+    else:
+        st.write("[Chat Mode]")
+        st.write(llm.invoke(chatPrompt.format(query=user_prompt)))
+    
+    st.write("API Data (Currently in development)")
+    st.write(output)
+
+capture_voice = st.button("Capture Voice")
+if capture_voice:
+    user_prompt = listen_to_microphone()
+    st.text_area("Enter voice prompt", value=user_prompt, key="voice_prompt")
+    process_user_input(user_prompt)
+else:
+    user_prompt = st.text_area("Enter your prompt")
+
 button = st.button("Submit")
 
 if button:
-    if user_prompt:
-        finalPrompt = prompt.format(query=user_prompt, format_instructions=format_instructions)
-        output = llm.invoke(finalPrompt)
-        data = output_parser.parse(output)
-
-        if data['action'] == 'SearchWeb':
-            st.write("Search Web for:", data['parameters'])
-            searchWeb(data['parameters'])
-        elif data['action'] == 'WriteCode':
-            st.write("Sure, I will write the code for you!")
-            codeOutput = llm.invoke(codePrompt.format(query=user_prompt))
-            st.write("Code API")
-            st.write(codeOutput)
-            lines = codeOutput.split('\n')
-            if lines and lines[0].startswith('`'): lines.pop(0)
-            if lines and lines[-1].startswith('`'): lines.pop()
-            codeOutput = '\n'.join(lines)
-            FileSystemManager.writeToFile("code.txt", codeOutput)
-        else:
-            st.write("[Chat Mode]")
-            st.write(llm.invoke(chatPrompt.format(query=user_prompt)))
-        
-        st.write("API Data (Currently in development)")
-        st.write(output)
+    if not user_prompt:
+        st.write("Please provide a prompt.")
+    else:
+        process_user_input(user_prompt)
